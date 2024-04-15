7fa15a2bca311dfe366e73ba8b68107454fb85d6
Alexandr <chestar.123@mail.ru>
2024-04-15 22:22:49 +0300

    Добавлена логика картинок в тестах

--- Полигон Для Шрд/Pages/7classpages/Test1/TestCompletePage.xaml.cs
+++ Полигон Для Шрд/Pages/7classpages/Test1/TestCompletePage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -33,10 +34,22 @@ namespace Полигон_Для_Шрд.Pages
             result = SaveResult.resultOfTest;
             db.ResultsOfTest.Add(result);
             db.SaveChanges();
+            int saveResultForImage = 0;
             var results = db.ResultsOfTest.Where(r => r.UserId == user.UserId && r.TestName == "Основы физики").ToList();
             foreach (var test in results)
             {
                 txtBlockResult.Text = $"Ваш результат: {test.Result} из 8";
+                saveResultForImage = test.Result;
+            }
+            if (saveResultForImage < 5) 
+            {
+                Uri uriImgaeBad = new Uri("/Images/ImagesTestCompleteBad.png", UriKind.RelativeOrAbsolute);
+                ImageResult.Source = new BitmapImage(uriImgaeBad);
+            }
+            else
+            {
+                Uri uriImgaeGood = new Uri("/Images/TestCompleteGood.png", UriKind.RelativeOrAbsolute);
+                ImageResult.Source = new BitmapImage(uriImgaeGood);
             }
         }
 
